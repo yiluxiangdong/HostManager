a883de3943168c8f45c1c0a193e52b73ac74ca9d
yiluxiangdong <2437375168@qq.com>
2018-06-22 10:56:27 +0800

    Update views.py

--- host/views.py
+++ host/views.py
@@ -10,7 +10,7 @@ from django.forms.models import model_to_dict
 # Create your views here.
 
 def  index(request):
-    # return render(request,'main.html',locals())
+   
     return render(request, 'index.html', locals())
 
 

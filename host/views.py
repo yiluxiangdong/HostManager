500b10247bd585848870910b0feb6b9f206a7279
liuxiaobing <2437375168@qq.com>
2018-06-22 10:53:47 +0800

    服务器管理

---
+++ host/views.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+from django.http import JsonResponse
+from django.shortcuts import render_to_response,redirect,HttpResponse,render,HttpResponseRedirect
+from .models import *
+import time,json,paramiko,ConfigParser,os
+from datetime  import datetime
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from django.forms.models import model_to_dict
+
+# Create your views here.
+
+def  index(request):
+    # return render(request,'main.html',locals())
+    return render(request, 'index.html', locals())
+
+
